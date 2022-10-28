ce62a98717b4cbc26375aa19ca46005ff527347b
Volodymyr <vovdata@gmail.com>
2022-10-28 19:06:27 +0200

    7
    7 fertig

---
+++ src/js/01-gallery.js
@@ -0,0 +1,40 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+// console.log(createMarkUp(galleryItems));
+
+const galleryContainer = document.querySelector('.gallery')
+const gMarkUp = createMarkUp(galleryItems)
+
+galleryContainer.insertAdjacentHTML('beforeend', gMarkUp)
+
+galleryContainer.addEventListener('click', onPictureClick);
+
+function createMarkUp(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+        .join('');
+}
+
+function onPictureClick(evt) {
+    evt.preventDefault();
+    const picSpace = evt.target.classList.contains('gallery__image')
+    if (!picSpace) {
+     return;   
+    } 
+    const instance = basicLightbox.create(`<img src='${evt.target.dataset.source}'>`)
+
+    instance.show()
+}   
+
+console.log(galleryItems);
